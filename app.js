0f91c146c2527f1fb2ae04456527c981354b466c
smdiare <smdedu2003@gmail.com>
2022-12-20 16:37:52 +0530

    second Add tests

--- app.js
+++ app.js
@@ -1,47 +1,64 @@
 /* eslint-disable no-unused-vars */
-const { request, response } = require("express");
 const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
+const path = require("path");
 app.use(bodyParser.json());
 
-app.get("/todos", (request, response) => {
-  // response.send('Hello World')
-  console.log("Todo List");
+app.set("view engine", "ejs");
+
+app.get("/", async (request, response) => {
+  const allTodos = await Todo.getTodo();
+  if (request.accepts("html")) {
+    response.render("index", { allTodos });
+  } else {
+    response.json({ allTodos });
+  }
+});
+
+// eslint-disable-next-line no-undef
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/todos", async (request, response) => {
+  const todoItems = await Todo.getTodo();
+  response.json(todoItems);
 });
 
 app.post("/todos", async (request, response) => {
-  console.log("creating a todo", request.body);
-
   try {
     const todo = await Todo.addTodo({
       title: request.body.title,
       dueDate: request.body.dueDate,
-      completed: false,
     });
     return response.json(todo);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return response.status(422).json(error);
   }
 });
 
 app.put("/todos/:id/markAsCompleted", async (request, response) => {
-  console.log("we have to update ID", request.params.id);
+  console.log("We have to update a todo with ID:", request.params.id);
   const todo = await Todo.findByPk(request.params.id);
-
   try {
     const updatedTodo = await todo.markAsCompleted();
     return response.json(updatedTodo);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return response.status(422).json(error);
   }
 });
 
-app.delete("/todos/:id", (request, response) => {
-  console.log("delete a todo by ID", request.params.id);
+app.delete("/todos/:id", async (request, response) => {
+  console.log("Delete a todo by ID:", request.params.id);
+  try {
+    const deletedTodo = await Todo.deleteTodo(request.params.id);
+    return response.send(deletedTodo ? true : false);
+  } catch (error) {
+    console.error(error);
+    return response.status(422).json(error);
+  }
 });
 
 module.exports = app;
