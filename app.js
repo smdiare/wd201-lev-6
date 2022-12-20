7b6cb7941f0307c76c7ef624dc74bd8e9fccd1e9
smdiare <smdedu2003@gmail.com>
2022-12-20 16:20:20 +0530

    Add tests

---
+++ app.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-unused-vars */
+const { request, response } = require("express");
+const express = require("express");
+const app = express();
+const { Todo } = require("./models");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.get("/todos", (request, response) => {
+  // response.send('Hello World')
+  console.log("Todo List");
+});
+
+app.post("/todos", async (request, response) => {
+  console.log("creating a todo", request.body);
+
+  try {
+    const todo = await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+      completed: false,
+    });
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.put("/todos/:id/markAsCompleted", async (request, response) => {
+  console.log("we have to update ID", request.params.id);
+  const todo = await Todo.findByPk(request.params.id);
+
+  try {
+    const updatedTodo = await todo.markAsCompleted();
+    return response.json(updatedTodo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.delete("/todos/:id", (request, response) => {
+  console.log("delete a todo by ID", request.params.id);
+});
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+const app = require("./app");
+app.listen(3000, () => {
+  console.log("started type localhost:3000/ in u r website");
+});
